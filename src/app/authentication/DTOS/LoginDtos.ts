37a2fbf75aabf79c363199974a688cbe45176485
DlopezS98 <01dlopezs98@gmail.com>
2023-10-21 11:34:50 -0600

    Add proxy config

--- src/app/authentication/DTOS/LoginDtos.ts
+++ src/app/authentication/DTOS/LoginDtos.ts
@@ -1,18 +1,16 @@
 import { JwtAuthDTO } from "./JwtAuthDtos";
 
-export class LoginRequestDTO
-{
+export class LoginRequestDTO {
 
     public username!: string;
 
-    public  password!: string;
+    public password!: string;
 }
 
-export class LoginResponseDTO
-{
-    public  username!: string;
-    public  email!:string;
+export class LoginResponseDTO {
+    public username!: string;
+    public email!: string;
     public roles: Array<string> = new Array<string>();
-    public jwt!:JwtAuthDTO;
+    public jwt!: JwtAuthDTO;
 }
 
--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { routes } from '../routes/routes';
 import { HttpClient } from '@angular/common/http';
 import { LoginRequestDTO, LoginResponseDTO } from 'src/app/authentication/DTOS/LoginDtos';
 import { RegisterRequestDTO, RegisterResponseDTO } from 'src/app/authentication/DTOS/RegisterDtos';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,12 +15,12 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   login(loginDto:LoginRequestDTO): Promise<LoginResponseDTO>{
-    const response = this.http.post<LoginResponseDTO>('https://localhost:7240/api/v1/authentication/login', loginDto);
+    const response = this.http.post<LoginResponseDTO>('/api/v1/authentication/login', loginDto).pipe(map((r: any) => r.data));
     return firstValueFrom(response);
   }
 
   registro(registroDto:RegisterRequestDTO): Promise<RegisterResponseDTO>{
-    const response = this.http.post<RegisterResponseDTO>('https://localhost:7240/api/v1/authentication/register', registroDto);
+    const response = this.http.post<RegisterResponseDTO>('/api/v1/authentication/register', registroDto);
     return firstValueFrom(response);
   }
 
